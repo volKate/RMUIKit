d48dad39aea00c10aa419ad6700adf26138568cb
Ekaterina V <katee.volkova@gmail.com>
2024-02-12 08:17:51 +0100

    refactor: replace images with vector; add docs and marks

--- UIKitTemplate/UIKitTemplate/AuthViewController.swift
+++ UIKitTemplate/UIKitTemplate/AuthViewController.swift
@@ -3,12 +3,16 @@
 
 import UIKit
 
-/// AuthViewController
-class AuthViewController: UIViewController {
-    @IBOutlet var emailField: UITextField!
-    @IBOutlet var passwordField: UITextField!
-    @IBOutlet var loginButton: UIButton!
-    @IBOutlet var eyeButton: UIButton!
+/// VC отображающий экран входа в приложение
+final class AuthViewController: UIViewController {
+    // MARK: - IBOutlets
+
+    @IBOutlet private var emailField: UITextField!
+    @IBOutlet private var passwordField: UITextField!
+    @IBOutlet private var loginButton: UIButton!
+    @IBOutlet private var eyeButton: UIButton!
+
+    // MARK: - Private Properties
 
     private var email: String = ""
     private var password: String = ""
@@ -17,10 +21,11 @@ class AuthViewController: UIViewController {
         email == "login@mail.ru" && password == "password"
     }
 
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        emailField.delegate = self
-        passwordField.delegate = self
+        setupView()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -29,19 +34,26 @@ class AuthViewController: UIViewController {
         loginButton.isEnabled = false
     }
 
-    func updateLoginButton() {
+    private func updateLoginButton() {
         if loginButton.isEnabled != isEnabled {
             loginButton.isEnabled = isEnabled
         }
     }
 
-    @IBAction func loginButtonTouched(_ sender: UIButton) {
+    // MARK: - Private Methods/@IBAction
+
+    private func setupView() {
+        emailField.delegate = self
+        passwordField.delegate = self
+    }
+
+    @IBAction private func loginButtonTouched(_ sender: UIButton) {
         if isEnabled {
             performSegue(withIdentifier: "loginSegue", sender: self)
         }
     }
 
-    @IBAction func eyeButtonTouched(_ sender: UIButton) {
+    @IBAction private func eyeButtonTouched(_ sender: UIButton) {
         passwordField.isSecureTextEntry.toggle()
         let eyeImage = UIImage(
             systemName: passwordField.isSecureTextEntry ? "eye.slash.fill" : "eye.fill",
@@ -52,6 +64,8 @@ class AuthViewController: UIViewController {
 }
 
 extension AuthViewController: UITextFieldDelegate {
+    // MARK: - Public Delegate Methods
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
--- UIKitTemplate/UIKitTemplate/CafeViewController.swift
+++ UIKitTemplate/UIKitTemplate/CafeViewController.swift
@@ -3,17 +3,27 @@
 
 import UIKit
 
-/// Cafe View
+/// VC запрашивающий у юзера информацию о пронировании
 class CafeViewController: UIViewController {
-    @IBOutlet var textFields: [UITextField]!
-    @IBOutlet var reservationSettings: [UISwitch]!
+    // MARK: - IBOutlets
+
+    @IBOutlet private var textFields: [UITextField]!
+    @IBOutlet private var reservationSettings: [UISwitch]!
+
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+
+    // MARK: - Private Methods/@IBAction
+
+    private func setupView() {
         textFields.forEach { $0.delegate = self }
     }
 
-    @IBAction func switchChangedValue(_ sender: UISwitch) {
+    @IBAction private func switchChangedValue(_ sender: UISwitch) {
         let allOn = reservationSettings.allSatisfy(\.isOn)
         print(allOn)
         if allOn {
@@ -21,7 +31,7 @@ class CafeViewController: UIViewController {
         }
     }
 
-    @IBAction func checkoutTouched(_ sender: Any) {
+    @IBAction private func checkoutTouched(_ sender: Any) {
         let alert = UIAlertController(title: "Выставить счет?", message: nil, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
         let checkoutAction = UIAlertAction(title: "Чек", style: .default) { [unowned self] _ in
@@ -36,6 +46,8 @@ class CafeViewController: UIViewController {
 }
 
 extension CafeViewController: UITextFieldDelegate {
+    // MARK: - Public Delegate Methods
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
